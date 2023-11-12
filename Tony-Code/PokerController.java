51a86655ca2972c67c80c8f9c4d8920630f07f5a
Anthony Hong <148808144+AirFuelRatio@users.noreply.github.com>
2023-11-11 19:24:49 -0700

    Update PokerController.java

--- Tony-Code/PokerController.java
+++ Tony-Code/PokerController.java
@@ -1,63 +1,3 @@
-//package application;
-//
-//import javafx.fxml.FXML;
-//import javafx.scene.control.Label;
-//import javafx.scene.control.TextField;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//public class PokerController {
-//
-//    @FXML
-//    private TextField pokerValueTextField;
-//    @FXML
-//    private Label resultLabel; // Add a label in FXML to display the result
-//    private int playerCount;
-//
-//    private final Map<Integer, Integer> playerVotes = new HashMap<>();
-//    private int currentPlayer = 1;
-//    
-//    public void setPlayerCount(int playerCount) {
-//        this.playerCount = playerCount;
-//        // Additional logic if necessary
-//    }
-//
-//    @FXML
-//    private void handlePassPlayerAction() {
-//        try {
-//            int value = Integer.parseInt(pokerValueTextField.getText());
-//            playerVotes.put(currentPlayer, value);
-//
-//            if (playerVotes.size() == 5) { // Check if all 5 players have entered their values
-//                if (allPlayersAgree()) {
-//                    resultLabel.setText("Agreed Value: " + value);
-//                } else {
-//                    resultLabel.setText("Players do not agree, continue...");
-//                    resetForNextRound();
-//                }
-//            } else {
-//                currentPlayer++;
-//                pokerValueTextField.clear(); // Clear the field for the next player
-//                resultLabel.setText("Player " + currentPlayer + ", enter your value.");
-//            }
-//        } catch (NumberFormatException e) {
-//            resultLabel.setText("Invalid input. Please enter a number.");
-//        }
-//    }
-//
-//    private boolean allPlayersAgree() {
-//        int firstValue = playerVotes.get(1);
-//        return playerVotes.values().stream().allMatch(v -> v == firstValue);
-//    }
-//
-//    private void resetForNextRound() {
-//        playerVotes.clear();
-//        currentPlayer = 1;
-//        pokerValueTextField.clear();
-//    }
-//}
-
-
 package application;
 
 import javafx.fxml.FXML;
@@ -90,9 +30,9 @@ public class PokerController {
 
             if (playerVotes.size() == playerCount) { // Check if all players have entered their values
                 if (allPlayersAgree()) {
-                    resultLabel.setText("Agreed Value: " + value);
+                    resultLabel.setText("Agreed Story Points Value: " + value);
                 } else {
-                    resultLabel.setText("Players do not agree, continue...");
+                    resultLabel.setText("Players do not agree, continue to next round!");
                     resetForNextRound();
                 }
             } else {
