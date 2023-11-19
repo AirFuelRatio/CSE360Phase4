4e3870d8eb56c39b6ac2d4317fca01aec5477887
Anthony Hong <148808144+AirFuelRatio@users.noreply.github.com>
2023-11-18 20:50:09 -0700

    Update EditController.java

--- Tony-Code/EditController.java
+++ Tony-Code/EditController.java
@@ -1,8 +1,13 @@
 package application;
 
+import application.Controller.Entry;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage; // TEST LINE
 
 public class EditController {
     @FXML
@@ -13,11 +18,50 @@ public class EditController {
     private ComboBox<String> effortComboBox1;
     @FXML
     private ComboBox<String> planComboBox1;
+    
+    private Entry editableEntry;
+    
+    public void populateFields(Entry entry) {
+        this.editableEntry = entry; // Store the reference to the Entry
+        projectComboBox1.setValue(entry.getProject());
+        lifecycleComboBox1.setValue(entry.getLifeCycle());
+        effortComboBox1.setValue(entry.getCategory());
+        planComboBox1.setValue(entry.getPlan());
+    }
+    
+    public void initialize() {
+    	projectComboBox1.setItems(FXCollections.observableArrayList("Business Project", "Development Project"));
+        lifecycleComboBox1.setItems(FXCollections.observableArrayList("Problem Understanding","Conceptual Design Plan",
+        "Requirements","Conceptual Design","Conceptual Design Review","Detailed Design Plan","Detailed Design/Prototype",
+        "Detailed Design Review","Implementation Plan","Test Case Generation","Solution Specification","Solution Review",
+        "Solution Implementation","Unit/System Test","Reflection","Repository Update","Planning","Information Gathering",
+        "Information Understanding","Verifying","Outlining","Drafting","Finalizing"));
+        effortComboBox1.setItems(FXCollections.observableArrayList("Plans", "Deliverables","Interruptions","Defects","Other"));
+        planComboBox1.setItems(FXCollections.observableArrayList("Project Plan", "Risk Management Plan", 
+        "Conceptual Design Plan","Detailed Design Plan", "Implementation Plan"));
+    }
+    
+    @FXML
+    private void handleApplyEdit() {
+        if (editableEntry != null) {
+            String selectedProject = projectComboBox1.getValue();
+            String selectedLifeCycle = lifecycleComboBox1.getValue();
+            String selectedEffort = effortComboBox1.getValue();
+            String selectedPlan = planComboBox1.getValue();
 
-    public void populateFields(SearchData searchData) {
-        projectComboBox1.setValue(searchData.getProject());
-        lifecycleComboBox1.setValue(searchData.getLifeCycle());
-        effortComboBox1.setValue(searchData.getEffort());
-        planComboBox1.setValue(searchData.getPlan());
+            // Update the entry with new values from combo boxes
+            editableEntry.setProject(selectedProject);
+            editableEntry.setLifeCycle(selectedLifeCycle);
+            editableEntry.setCategory(selectedEffort);
+            editableEntry.setPlan(selectedPlan);
+            
+            closeEditWindow();
+        }
+    }
+    
+    private void closeEditWindow() {
+        // Get the current stage using one of the controls
+        Stage stage = (Stage) projectComboBox1.getScene().getWindow();
+        stage.close();
     }
 }
